e73070625fe7c8dc77face22adaf519d48974d62
Student User <student@workstation.lab.example.com>
2019-09-18 19:30:08 +0000

    second update

--- php-scale/index.php
+++ php-scale/index.php
@@ -1,3 +1,3 @@
 <?php
-  print "This is version 2 of the app. I am running on second host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+  print "This is version 3 of the app. I am running on third second host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
 ?>
